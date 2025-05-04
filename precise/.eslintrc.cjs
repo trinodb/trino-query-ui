894ed4475b27affd4727f6df65da97f8375187d0
Peter Kosztolanyi <peter.kosztolanyi@gmail.com>
2025-05-04 20:59:34 +0200

    Add eslint and prettier config

--- precise/.eslintrc.cjs
+++ precise/.eslintrc.cjs
@@ -14,5 +14,10 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    // ❗ Temporarily disabled due to widespread use of `any` an `unused vars` in the codebase.
+    //    Defining proper types will require significant effort.
+    //    We will address this incrementally in future pull requests.
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
   },
 }
