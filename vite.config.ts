879f83a80bfd2082bd66344d9717e7779282de3d
Peter Kosztolanyi <peter.kosztolanyi@gmail.com>
2026-09-24 21:39:47 +0200

    Remove obsolete Trino integration instructions from README.md
    Document the current authentication limitation and link to github issue for details on planned Web UI session reuse

--- vite.config.ts
+++ vite.config.ts
@@ -41,10 +41,3 @@ export default defineConfig({
     },
   },
 })
-
-// Used for integration into Trino
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   base: '/query/', // This tells your app it's served from the /query/ path
-//   plugins: [react()]
-// });
